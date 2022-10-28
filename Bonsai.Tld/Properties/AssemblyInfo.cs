f75ca28715112bb0651437e0a409aba92707c6b2
glopesdev <glopesdev@users.noreply.github.com>
2022-10-28 18:47:29 +0100

    Update OpenCV and package specification files

--- Bonsai.Tld/Properties/AssemblyInfo.cs
+++ Bonsai.Tld/Properties/AssemblyInfo.cs
@@ -1,62 +1,7 @@
-﻿/*  Copyright 2015 Gonçalo Lopes
-*
-*   This file is part of OpenTLD.NET.
-*
-*   OpenTLD.NET is free software: you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   OpenTLD.NET is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with OpenTLD.NET.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
-/*
- * AssemblyInfo.cs
- *
- *  Created on: Nov 16, 2015
- *      Author: Gonçalo Lopes
- */
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using Bonsai;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bonsai - TLD Library")]
-[assembly: AssemblyDescription("Bonsai Tracking Library containing the TLD self-supervised tracking algorithm.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gonçalo Lopes")]
-[assembly: AssemblyProduct("Bonsai.Tld")]
-[assembly: AssemblyCopyright("Copyright © Gonçalo Lopes 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("697c4149-bcd3-48be-a050-0aaf99b8d338")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: XmlNamespacePrefix("clr-namespace:Bonsai.Tld", "tld")]
+[assembly: WorkflowNamespaceIcon("Bonsai:ElementIcon.Vision")]
